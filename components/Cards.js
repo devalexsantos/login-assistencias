953af4f054eccfc9386a3a066dee855f7c9b90cc
Alex Santos <devalexsantos@gmail.com>
2022-05-15 11:17:06 -0300

    Adicionando Logo e estilos

--- components/Cards.js
+++ components/Cards.js
@@ -1,6 +1,6 @@
 export default function Cards() {
   return (
-    <div className="m-auto sm:max-w-[250px] flex flex-col justify-center gap-1 text-cyan-900 shadow-lg p-2 m-4 rounded">
+    <div className="m-auto sm:max-w-[250px] flex flex-col justify-center gap-1 text-cyan-900 shadow-lg p-2 m-4 rounded bg-white">
       <h1 className="font-bold text-md leading-5">
         Barbara Correia Martins Informática Barbara Correia Martins Informática
       </h1>
--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,18 @@
 import { ArrowBendUpRight } from "phosphor-react";
+import Image from "next/image";
 
 export default function Header() {
   return (
     <div className="w-auto text-center bg-cyan-900 py-6">
       <div className="m-auto container mx-auto flex flex-col justify-center align-center gap-6">
-        <div className="flex flex-col gap-2 md:gap-0 justify-center md:flex-row md:justify-between text-white py-4 md:py-0">
-          <span>Logo</span>
-          <button className="flex justify-center gap-2 align-center p-1 bg-white text-cyan-900 rounded w-[70%] md:w-auto m-auto md:m-0 md:px-4">
+        <div className="flex flex-col items-center gap-2 md:gap-0 justify-center md:flex-row md:justify-between md:items-center text-white py-4 md:py-0">
+          <Image
+            src="/assets/logo-white-web.svg"
+            alt="Logo Login"
+            width={150}
+            height={"100%"}
+          />
+          <button className="flex justify-center gap-2 items-center p-1 bg-white text-cyan-900 rounded h-[100%] w-[50%] md:w-auto">
             <ArrowBendUpRight size={24} weight="bold" /> Ir para o Site
           </button>
         </div>
@@ -18,7 +24,7 @@ export default function Header() {
             Selecione seu estado:
           </h2>
           <div className="text-black">
-            <select className="p-2 rounded outline-none w-[80%] md:w-[200px]">
+            <select className="p-2 rounded outline-none w-[50%] md:w-[200px]">
               <option value="1">Bahia</option>
               <option value="2">Sergipe</option>
             </select>
